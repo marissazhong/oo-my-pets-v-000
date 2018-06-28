f72665b16fb676b3772a5b40dc9c9c3713d43234
Marissa Zhong <mwz2101@columbia.edu>
2018-06-28 22:23:44 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -48,11 +48,11 @@ class Owner
   end
 
   def sell_pets
-    @pets.values.each {|pet| pet.mood = "nervous"}
+    @pets.values.map {|pet| pet.mood = "nervous"}
   end
 
   def list_pets
-    @pets.values.each {|pet| puts pet.name}
+    @pets.values.map {|pet| puts pet.name}
   end
 
   def self.count
